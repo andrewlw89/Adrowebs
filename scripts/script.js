d168807f165ae857b1e574b2abb560e1b5897fbd
Andrew Williams <andrewleighwilliams@gmail.com>
2015-07-01 16:15:42 -0400

    initial commit. Begin styling main page. Next up: implement flickr api

---
+++ scripts/script.js
@@ -0,0 +1,13 @@
+
+
+
+<?xml version="1.0" encoding="utf-8" ?>
+<rsp stat="ok">
+  <photos page="1" pages="1" perpage="100" total="4">
+    <photo id="18711625203" owner="132728132@N04" secret="fd8fe67cdf" server="544" farm="1" title="frogger" ispublic="1" isfriend="0" isfamily="0" />
+    <photo id="19332225695" owner="132728132@N04" secret="7dfbd55e8e" server="425" farm="1" title="feedreader" ispublic="1" isfriend="0" isfamily="0" />
+    <photo id="19144624038" owner="132728132@N04" secret="1f340ff28e" server="264" farm="1" title="dankwatch" ispublic="1" isfriend="0" isfamily="0" />
+    <photo id="18709714364" owner="132728132@N04" secret="a8fdf6006c" server="260" farm="1" title="map-project" ispublic="1" isfriend="0" isfamily="0" />
+  </photos>
+</rsp>
+
