49ab675a4e42297862eb5fbd97b065e3124e147a
Andrew Williams <andrewleighwilliams@gmail.com>
2015-07-01 18:07:08 -0400

    successfully making flickr API call

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,13 @@
+$.ajax({ 
+   type: "GET",
+   dataType: "jsonp",
+   url: "https://api.flickr.com/services/rest/?method=flickr.people.getPublicPhotos&api_key=69856db44f32145ff44c1243b55bd615&user_id=132728132%40N04&format=json"
+});
+
+function jsonFlickrApi(response) {
+	console.log(response);
 
 
+}
 
-<?xml version="1.0" encoding="utf-8" ?>
-<rsp stat="ok">
-  <photos page="1" pages="1" perpage="100" total="4">
-    <photo id="18711625203" owner="132728132@N04" secret="fd8fe67cdf" server="544" farm="1" title="frogger" ispublic="1" isfriend="0" isfamily="0" />
-    <photo id="19332225695" owner="132728132@N04" secret="7dfbd55e8e" server="425" farm="1" title="feedreader" ispublic="1" isfriend="0" isfamily="0" />
-    <photo id="19144624038" owner="132728132@N04" secret="1f340ff28e" server="264" farm="1" title="dankwatch" ispublic="1" isfriend="0" isfamily="0" />
-    <photo id="18709714364" owner="132728132@N04" secret="a8fdf6006c" server="260" farm="1" title="map-project" ispublic="1" isfriend="0" isfamily="0" />
-  </photos>
-</rsp>
-
+//https://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}.jpg
